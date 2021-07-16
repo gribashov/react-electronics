1071a715e28cd555048ffc7ece8d052ef9dbce3c
gribashov <gribashovdeveloper@gmail.com>
2021-07-16 20:44:12 +0300

    added mocAPI server using fetch

--- src/App.js
+++ src/App.js
@@ -6,31 +6,15 @@ import Basket from "./components/Basket";
 import Banner from "./components/Banner";
 import Search from "./components/Search";
 
-const arr = [
-  {
-    title: "Умные часы Huawei 12 Pro Max",
-    price: "14 320",
-    img: "/img/product-1.jpg",
-  },
-  {
-    title: "Bluetooth наушники Beats Solo Blue",
-    price: "12 999",
-    img: "/img/product-2.jpg",
-  },
-  {
-    title: "Игровые наушники Blazer Gaming Plus",
-    price: "11 390",
-    img: "/img/product-3.jpg",
-  },
-  {
-    title: "Офисный ноутбук Matepad Lumix 3000",
-    price: "36 790",
-    img: "/img/product-4.jpg",
-  },
-];
-
 function App() {
+  const [goods, setGoods] = React.useState([]);
   const [isVisibleBasket, setIsVisibleBasket] = React.useState(false);
+
+  fetch("https://60f1ba8c38ecdf0017b0fda4.mockapi.io/goods")
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => setGoods(json));
 
   return (
     <div className="App">
@@ -59,7 +43,7 @@ function App() {
         {/* cards arr */}
         <div className="flex items-center justify-between flex-wrap pr-10">
           {/* card item */}
-          {arr.map((obj, index) => (
+          {goods.map((obj, index) => (
             <Card
               key={index}
               title={obj.title}
