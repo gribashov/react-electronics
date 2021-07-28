cc66063c8df5a830982b00562081d4ad050dc08c
gribashov <gribashovdeveloper@gmail.com>
2021-07-28 20:02:33 +0300

    add link for back button in Orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 import {Order} from "./../components/Order";
 import Info from "./../components/Info";
@@ -22,23 +23,25 @@ function Orders() {
   return (
     <div className="my-10">
       <div className="flex items-center mx-10">
-        <div className="w-8 h-8 flex justify-center items-center bg-black rounded-lg mr-4 cursor-pointer">
-          <svg
-            width="7"
-            height="12"
-            viewBox="0 0 7 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M6 11L1 6L6 1"
-              stroke="white"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
+        <Link to="/">
+          <div className="w-8 h-8 flex justify-center items-center bg-black rounded-lg mr-4 cursor-pointer">
+            <svg
+              width="7"
+              height="12"
+              viewBox="0 0 7 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6 11L1 6L6 1"
+                stroke="white"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        </Link>
         <div className="font-bold text-3xl pr-2 truncate">Мои покупки</div>
       </div>
       {/* orders arr */}
